c0a13b876dbb23f393372511ad8eb61be811d197
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2019-12-05 19:24:27 -0600

    Updates

---
+++ precache-manifest.5a43ba652f299645c23db3e24c19998e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb55d0fd8a6b3eda603cfff4adfe256",
+    "url": "/CN--Nimanic-Dex/index.html"
+  },
+  {
+    "revision": "414788e394a0aafdce6f",
+    "url": "/CN--Nimanic-Dex/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "434a8c959f1f0d3372e2",
+    "url": "/CN--Nimanic-Dex/static/css/main.c79c666c.chunk.css"
+  },
+  {
+    "revision": "414788e394a0aafdce6f",
+    "url": "/CN--Nimanic-Dex/static/js/2.b3bc5e4d.chunk.js"
+  },
+  {
+    "revision": "434a8c959f1f0d3372e2",
+    "url": "/CN--Nimanic-Dex/static/js/main.a69b5ff3.chunk.js"
+  },
+  {
+    "revision": "08734176334d012532c1",
+    "url": "/CN--Nimanic-Dex/static/js/runtime-main.6aaf7612.js"
+  }
+]);
